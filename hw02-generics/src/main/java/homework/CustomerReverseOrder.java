fc61ef153c32da87c1118930d46b4c2cad10288e
s.soloviov <soloviov-sergey@yandex.ru>
2023-07-22 20:29:12 +0300

    [hw02-generics] Объявил поле как Deque

--- hw02-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw02-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,12 +1,13 @@
 package homework;
 
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class CustomerReverseOrder {
 
     // todo: 2. надо реализовать методы этого класса
     // надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
-    private final LinkedList<Customer> customers = new LinkedList<>();
+    private final Deque<Customer> customers = new LinkedList<>();
 
     public void add(Customer customer) {
         customers.addFirst(customer);
