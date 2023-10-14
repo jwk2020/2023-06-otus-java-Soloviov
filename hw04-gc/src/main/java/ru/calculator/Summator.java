ebc0b281ffda4f339fc71f267cb337ca567547c1
s.soloviov <soloviov-sergey@yandex.ru>
2023-10-14 11:49:49 +0300

    [hw04-gc] Определение нужного размера хипа

--- hw04-gc/src/main/java/ru/calculator/Summator.java
+++ hw04-gc/src/main/java/ru/calculator/Summator.java
@@ -4,49 +4,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Summator {
-    private Integer sum = 0;
-    private Integer prevValue = 0;
-    private Integer prevPrevValue = 0;
-    private Integer sumLastThreeValues = 0;
-    private Integer someValue = 0;
-    private final List<Data> listValues = new ArrayList<>();
+    private final int MAX_SIZE = 6_600_000;
+    private int sum = 0;
+    private int prevValue = 0;
+    private int prevPrevValue = 0;
+    private int sumLastThreeValues = 0;
+    private int someValue = 0;
+    private final List<Data> listValues = new ArrayList<>(MAX_SIZE);
 
     // !!! сигнатуру метода менять нельзя
     public void calc(Data data) {
         listValues.add(data);
-        if (listValues.size() % 6_600_000 == 0) {
+        if (listValues.size() == MAX_SIZE) {
             listValues.clear();
         }
-        sum += data.getValue();
+        int value = data.getValue();
+        sum += value;
 
-        sumLastThreeValues = data.getValue() + prevValue + prevPrevValue;
+        sumLastThreeValues = value + prevValue + prevPrevValue;
 
         prevPrevValue = prevValue;
-        prevValue = data.getValue();
+        prevValue = value;
 
+        int calculation = sumLastThreeValues * sumLastThreeValues / (value + 1) - sum;
         for (var idx = 0; idx < 3; idx++) {
-            someValue += (sumLastThreeValues * sumLastThreeValues / (data.getValue() + 1) - sum);
+            someValue += calculation;
             someValue = Math.abs(someValue) + listValues.size();
         }
     }
 
-    public Integer getSum() {
+    public int getSum() {
         return sum;
     }
 
-    public Integer getPrevValue() {
+    public int getPrevValue() {
         return prevValue;
     }
 
-    public Integer getPrevPrevValue() {
+    public int getPrevPrevValue() {
         return prevPrevValue;
     }
 
-    public Integer getSumLastThreeValues() {
+    public int getSumLastThreeValues() {
         return sumLastThreeValues;
     }
 
-    public Integer getSomeValue() {
+    public int getSomeValue() {
         return someValue;
     }
 }
