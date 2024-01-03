2664e8e2aa4fd7632a99b1686a87cc581d8fe608
jwk2020 <74426969+jwk2020@users.noreply.github.com>
2024-01-03 23:33:31 +0300

    Hw08 io (#10)
    * [hw08-io] initial
    
    * [hw08-io] Обработчик json-ов
    
    ---------
    
    Co-authored-by: s.soloviov <soloviov-sergey@yandex.ru>

---
+++ hw08-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -0,0 +1,28 @@
+package ru.otus.dataprocessor;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ru.otus.model.Measurement;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ResourcesFileLoader implements Loader {
+
+    private final String fileName;
+
+    public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public List<Measurement> load() {
+        // читает файл, парсит и возвращает результат
+        try {
+            return new ObjectMapper().readValue(ClassLoader.getSystemResource(fileName), new TypeReference<>() {
+            });
+        } catch (IOException e) {
+            throw new FileProcessException(e.getMessage());
+        }
+    }
+}
