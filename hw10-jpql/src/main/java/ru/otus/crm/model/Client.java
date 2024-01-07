c902f1d1dec1bf8b229b59e4dc8ac8b4a7502744
s.soloviov <soloviov-sergey@yandex.ru>
2024-01-07 14:00:57 +0300

    [hw10-jpql] Использование Hibernate

--- hw10-jpql/src/main/java/ru/otus/crm/model/Client.java
+++ hw10-jpql/src/main/java/ru/otus/crm/model/Client.java
@@ -1,16 +1,25 @@
 package ru.otus.crm.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
-import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.List.copyOf;
 
 @Getter
 @Setter
@@ -28,6 +37,13 @@ public class Client implements Cloneable {
     @Column(name = "name")
     private String name;
 
+    @OneToOne(cascade = {CascadeType.ALL}, orphanRemoval = true)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @OneToMany(cascade = {CascadeType.ALL}, fetch = FetchType.LAZY, orphanRemoval = true, mappedBy = "client")
+    private List<Phone> phones = new ArrayList<>();
+
     public Client(String name) {
         this.id = null;
         this.name = name;
@@ -38,18 +54,34 @@ public class Client implements Cloneable {
         this.name = name;
     }
 
-    public <E> Client(Long id, String name, Address address, List<Phone> phones) {
-        throw new UnsupportedOperationException();
+    public Client(Long id, String name, Address address, List<Phone> phones) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        if (phones != null) {
+            phones.forEach(phone -> phone.setClient(this));
+            this.phones.addAll(phones);
+        }
     }
 
     @Override
     @SuppressWarnings({"java:S2975", "java:S1182"})
     public Client clone() {
-        return new Client(this.id, this.name);
+        return new Client(
+                this.id,
+                this.name,
+                this.address != null ? this.address.clone() : null,
+                this.phones != null ? copyOf(this.phones) : null
+        );
     }
 
     @Override
     public String toString() {
-        return "Client{" + "id=" + id + ", name='" + name + '\'' + '}';
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                ", phones=" + phones +
+                '}';
     }
 }
