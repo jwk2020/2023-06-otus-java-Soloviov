d8db0696169b7e132044ab98a65b5146690eda07
s.soloviov <soloviov-sergey@yandex.ru>
2024-02-01 08:13:53 +0300

    [hw15-sequenceOfNumbers] Последовательность чисел

---
+++ hw15-sequenceOfNumbers/src/main/java/ru/otus/SequencePrinter.java
@@ -0,0 +1,59 @@
+package ru.otus;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SequencePrinter {
+    private String last = "THREAD 2";
+
+    public static void main(String[] args) {
+        new SequencePrinter().go();
+    }
+
+    private void go() {
+        new Thread(this::print, "THREAD 1").start();
+        new Thread(this::print, "THREAD 2").start();
+    }
+
+    private synchronized void print() {
+        var sequenceGenerator = new SequenceGenerator();
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                while (last.equals(Thread.currentThread().getName())) {
+                    this.wait();
+                }
+                last = Thread.currentThread().getName();
+                log.info(sequenceGenerator.getNext());
+                sleep();
+                this.notifyAll();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    private static void sleep() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private static class SequenceGenerator {
+        private static final int MIN = 1;
+        private static final int MAX = 10;
+        private int number = MIN;
+        private boolean increase = true;
+
+        public String getNext() {
+            if (number == MAX) {
+                increase = false;
+            } else if (number == MIN) {
+                increase = true;
+            }
+            return String.valueOf(increase ? number++ : number--);
+        }
+    }
+}
