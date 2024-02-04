0d5b6a9da538a4de50567c1276f772f65956a59a
s.soloviov <soloviov-sergey@yandex.ru>
2024-02-04 09:53:50 +0300

    [hw16-queues] Queues

--- hw16-queues/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
+++ hw16-queues/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -6,6 +6,12 @@ import ru.otus.api.SensorDataProcessor;
 import ru.otus.api.model.SensorData;
 import ru.otus.lib.SensorDataBufferedWriter;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+
 // Этот класс нужно реализовать
 @SuppressWarnings({"java:S1068", "java:S125"})
 public class SensorDataProcessorBuffered implements SensorDataProcessor {
@@ -13,24 +19,30 @@ public class SensorDataProcessorBuffered implements SensorDataProcessor {
 
     private final int bufferSize;
     private final SensorDataBufferedWriter writer;
+    private final BlockingQueue<SensorData> dataBuffer;
 
     public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
         this.bufferSize = bufferSize;
         this.writer = writer;
+        this.dataBuffer = new PriorityBlockingQueue<>(bufferSize, Comparator.comparing(SensorData::getMeasurementTime));
     }
 
     @Override
     public void process(SensorData data) {
-        /*
-            if (dataBuffer.size() >= bufferSize) {
-                flush();
-            }
-        */
+        dataBuffer.add(data);
+        if (dataBuffer.size() >= bufferSize) {
+            flush();
+        }
     }
 
     public void flush() {
         try {
-            // writer.writeBufferedData(bufferedData);
+            if (!dataBuffer.isEmpty()) {
+                List<SensorData> bufferedData = new ArrayList<>();
+                if (dataBuffer.drainTo(bufferedData) > 0) {
+                    writer.writeBufferedData(bufferedData);
+                }
+            }
         } catch (Exception e) {
             log.error("Ошибка в процессе записи буфера", e);
         }
